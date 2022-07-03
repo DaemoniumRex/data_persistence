d12c6c0d9259dc5582864d772296b03868d149e0
DaemoniumRex <maogalvis@yahoo.com>
2022-07-03 10:15:51 -0500

    v0.1
    Pong test, some mats and movement of the paddle

---
+++ Assets/Scripts/PongBall.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongBall : MonoBehaviour
+{
+    private Rigidbody spirit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spirit = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StarDir()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PongPaddle.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongPaddle : MonoBehaviour
+{
+     public float Speed = 2.0f;
+    public float MaxMovement = 5.9f;
+    public float MinMovement = -3.66f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float input = Input.GetAxis("V2");
+
+         Vector3 pos = transform.position;
+
+         
+        pos.y += input * Speed * Time.deltaTime;
+
+        if (pos.y > MaxMovement)
+            pos.y = MaxMovement;
+        else if (pos.y < MinMovement)
+            pos.y = MinMovement;
+
+         transform.position = pos;
+    }
+}
